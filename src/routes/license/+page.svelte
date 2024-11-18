7781a2927c686a5f3fed20c1579eaf4d4652e785
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-18 21:14:38 +0900

    만들다 맘

---
+++ src/routes/license/+page.svelte
@@ -0,0 +1,16 @@
+<template>
+  <h1>that seed</h1>
+  <h2>Version: v4.25.2</h2>
+    <ul>
+      <li>Backend: </li>
+      <li>Frontend: </li>
+      <li>Skin: <li>
+    </ul>
+  <h1>Contributors</h1>
+    <ul>
+      <li><a href="//github.com/1ongames">iongames</a> (frontend & backend)</li>
+      <li><a href="//github.com/uiovasot">uiovasot</a> (backend & render)
+    </ul>
+  <h2>Super Thanks</h2>
+  <h1>Open Source License</h1>
+</template>
