2333c769f3af6ed20eaa0c20ed3d2fb04605fdbb
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-21 18:48:36 +0900

    Update +page.svelte

--- src/routes/license/+page.svelte
+++ src/routes/license/+page.svelte
@@ -1,10 +1,10 @@
 <template>
   <h1>that seed</h1>
-  <h2>Version: v4.25.2</h2>
+  <h2>Version: v{engine_v}/h2>
     <ul>
-      <li>Backend: </li>
-      <li>Frontend: </li>
-      <li>Skin: <li>
+      <li>Backend: v{back_v}</li>
+      <li>Frontend: {front_v}</li>
+      <li>Skin: {skin_v}<li>
     </ul>
   <h1>Contributors</h1>
     <ul>
@@ -14,3 +14,10 @@
   <h2>Super Thanks</h2>
   <h1>Open Source License</h1>
 </template>
+
+<script lang="ts">
+  export let engine_v: string;
+  export let back_v: string;
+  export let front_v: string;
+  export let skin_v: string;
+</script>
